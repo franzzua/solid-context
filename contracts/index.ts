3e9241d8e00c56ae1b77ecdc6989c395f6dbcd5d
fransua <andrey-ne@mail.ru>
2020-05-26 21:58:45 +0300

    f

--- contracts/index.ts
+++
@@ -1,9 +0,0 @@
-import { TripleDocument, TripleSubject, Reference,  } from "tripledoc";
-
-export * from "./file.service";
-export * from "./api";
-export {
-    TripleDocument,
-    TripleSubject,
-    Reference
-}
